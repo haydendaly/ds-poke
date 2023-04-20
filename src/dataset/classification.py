55fbc57fd81b983c60168a0e2c57da01e0d602e6
haydendaly <hcd36@cornell.edu>
2023-04-19 23:58:00 -0400

    add one shot ex

--- src/dataset/classification.py
+++ src/dataset/classification.py
@@ -1,9 +1,17 @@
 from src.shared.storage import Database, DataFrameStorage
 
 
-def get_classification_df():
+def get_oneshot_classification_df():
     classification_df = DataFrameStorage("classification", db=Database.SHARED).get(
         "combined"
+    )
+    classification_df = classification_df.set_index("id")
+    return classification_df
+
+
+def get_full_classification_df():
+    classification_df = DataFrameStorage("classification", db=Database.SHARED).get(
+        "full"
     )
     classification_df = classification_df.set_index("id")
     return classification_df
